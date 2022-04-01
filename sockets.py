b437aacba87295313995d53c636e49bdb079b7c0
Giancarlo Pernudi Segura <giancarlopernudi@hotmail.com>
2022-03-31 22:56:13 -0600

    progress

--- sockets.py
+++ sockets.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+from html import entities
 import flask
 from flask import Flask, request
 from flask_sockets import Sockets
@@ -21,32 +22,34 @@ from gevent import queue
 import time
 import json
 import os
+from http import HTTPStatus
 
 app = Flask(__name__)
 sockets = Sockets(app)
 app.debug = True
+
 
 class World:
     def __init__(self):
         self.clear()
         # we've got listeners now!
         self.listeners = list()
-        
+
     def add_set_listener(self, listener):
-        self.listeners.append( listener )
+        self.listeners.append(listener)
 
     def update(self, entity, key, value):
-        entry = self.space.get(entity,dict())
+        entry = self.space.get(entity, dict())
         entry[key] = value
         self.space[entity] = entry
-        self.update_listeners( entity )
+        self.update_listeners(entity)
 
     def set(self, entity, data):
         self.space[entity] = data
-        self.update_listeners( entity )
+        self.update_listeners(entity)
 
     def update_listeners(self, entity):
-        '''update the set listeners'''
+        """update the set listeners"""
         for listener in self.listeners:
             listener(entity, self.get(entity))
 
@@ -54,75 +57,115 @@ class World:
         self.space = dict()
 
     def get(self, entity):
-        return self.space.get(entity,dict())
-    
+        return self.space.get(entity, dict())
+
     def world(self):
         return self.space
 
-myWorld = World()        
 
-def set_listener( entity, data ):
-    ''' do something with the update ! '''
+class Client:
+    def __init__(self):
+        self.queue = queue.Queue()
 
-myWorld.add_set_listener( set_listener )
-        
-@app.route('/')
+    def put(self, v):
+        self.queue.put_nowait(v)
+
+    def get(self):
+        return self.queue.get()
+
+
+myWorld = World()
+clients = []
+
+
+def set_listener(entity, data):
+    """do something with the update !"""
+
+
+myWorld.add_set_listener(set_listener)
+
+
+@app.route("/")
 def hello():
-    '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    """Return something coherent here.. perhaps redirect to /static/index.html"""
+    return app.send_static_file("index.html"), HTTPStatus.OK
 
-def read_ws(ws,client):
-    '''A greenlet function that reads from the websocket and updates the world'''
-    # XXX: TODO IMPLEMENT ME
-    return None
 
-@sockets.route('/subscribe')
+def read_ws(ws, client):
+    """A greenlet function that reads from the websocket and updates the world"""
+    msg = ws.receive()
+    ws.send(msg)
+    while msg:
+        entities = json.loads(msg)
+        for entity, d in entities.items:
+            myWorld.set(entity, d)
+        msg = ws.receive()
+    return {}
+
+
+@sockets.route("/subscribe")
 def subscribe_socket(ws):
-    '''Fufill the websocket URL of /subscribe, every update notify the
-       websocket and read updates from the websocket '''
-    # XXX: TODO IMPLEMENT ME
-    return None
+    """Fufill the websocket URL of /subscribe, every update notify the
+    websocket and read updates from the websocket"""
+    client = Client()
+    clients.append(client)
+    g = gevent.spawn(read_ws, ws, client)
+    try:
+        while True:
+            msg = client.get()
+            ws.send(msg)
+    except Exception as e:
+        print(e)
+    finally:
+        clients.remove(client)
+        gevent.kill(g)
+    return {}
 
 
 # I give this to you, this is how you get the raw body/data portion of a post in flask
 # this should come with flask but whatever, it's not my project.
 def flask_post_json():
-    '''Ah the joys of frameworks! They do so much work for you
-       that they get in the way of sane operation!'''
-    if (request.json != None):
+    """Ah the joys of frameworks! They do so much work for you
+    that they get in the way of sane operation!"""
+    if request.json != None:
         return request.json
-    elif (request.data != None and request.data.decode("utf8") != u''):
+    elif request.data != None and request.data.decode("utf8") != "":
         return json.loads(request.data.decode("utf8"))
     else:
         return json.loads(request.form.keys()[0])
 
-@app.route("/entity/<entity>", methods=['POST','PUT'])
+
+@app.route("/entity/<entity>", methods=["POST", "PUT"])
 def update(entity):
-    '''update the entities via this interface'''
-    return None
+    """update the entities via this interface"""
+    data = flask_post_json()
+    myWorld.set(entity, data)
+    return {}, HTTPStatus.OK
 
-@app.route("/world", methods=['POST','GET'])    
+
+@app.route("/world", methods=["POST", "GET"])
 def world():
-    '''you should probably return the world here'''
-    return None
+    """you should probably return the world here"""
+    return myWorld.world(), HTTPStatus.OK
 
-@app.route("/entity/<entity>")    
+
+@app.route("/entity/<entity>")
 def get_entity(entity):
-    '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    """This is the GET version of the entity interface, return a representation of the entity"""
+    return myWorld.get(entity), HTTPStatus.OK
 
 
-@app.route("/clear", methods=['POST','GET'])
+@app.route("/clear", methods=["POST", "GET"])
 def clear():
-    '''Clear the world out!'''
-    return None
-
+    """Clear the world out!"""
+    myWorld.clear()
+    return {}, HTTPStatus.OK
 
 
 if __name__ == "__main__":
-    ''' This doesn't work well anymore:
-        pip install gunicorn
-        and run
-        gunicorn -k flask_sockets.worker sockets:app
-    '''
+    """This doesn't work well anymore:
+    pip install gunicorn
+    and run
+    gunicorn -k flask_sockets.worker sockets:app
+    """
     app.run()
